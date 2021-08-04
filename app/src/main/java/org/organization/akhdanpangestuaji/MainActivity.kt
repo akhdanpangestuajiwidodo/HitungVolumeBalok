e365f430bc07409213388bfa25e8a1cbca31ffb6
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-08-04 08:32:41 +0700

    create activity logic

--- app/src/main/java/org/organization/akhdanpangestuaji/MainActivity.kt
+++ app/src/main/java/org/organization/akhdanpangestuaji/MainActivity.kt
@@ -3,11 +3,29 @@ package org.organization.akhdanpangestuaji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import org.organization.akhdanpangestuaji.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalculate.setOnClickListener { calculateVolume() }
+    }
+
+    private fun calculateVolume() {
+
+        val length = binding.edtLength.text.toString().toFloat()
+        val height = binding.edtHeight.text.toString().toFloat()
+        val width = binding.edtWidth.text.toString().toFloat()
+
+        val volume = length * height * width
+        binding.tvResult.text = volume.toString()
+
     }
 }
+
