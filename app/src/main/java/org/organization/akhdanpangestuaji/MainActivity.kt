1a3eb0babfd727b25b5b2b2d2dd1e5a4d9074d74
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-08-04 08:01:43 +0700

    add viewbinding library

--- app/src/main/java/org/organization/akhdanpangestuaji/MainActivity.kt
+++ app/src/main/java/org/organization/akhdanpangestuaji/MainActivity.kt
@@ -2,8 +2,10 @@ package org.organization.akhdanpangestuaji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
